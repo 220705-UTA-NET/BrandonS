ac00dfdbb9f9abe9bf2ca34ae420e73c66e4bcb6
bsassano <bsassano@calpoly.edu>
2022-07-06 12:25:15 -0700

    Committing Dotnet Code

---
+++ MyFirstDotnet/MyFirstDotnet/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+// This is a comment in .Net
+
+// Use libraries and other classes to extract code that accomplishes complex functions quickly
+
+// Console.WriteLine is from the System library and allows us to write a line of text to the console
+// (___) we are sending the preceding thing into the function. This is a parameter.
+// "" signifies a string literal
+// ; Endline character
+
+Console.WriteLine("Hello, World!");
